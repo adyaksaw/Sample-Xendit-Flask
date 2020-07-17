cb6c0a5accec2b7fd6cf546dde7790a042077569
Firastic <adyaksa.wisanggeni@gmail.com>
2020-07-17 09:40:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Sample Xendit Flask
+
+To use it, you need Poetry as package manager.
+
+After setting the API Key in `XENDIT_API_KEY`, run `python app.py`.
---
+++ app.py
@@ -0,0 +1,35 @@
+import os
+import time
+
+from xendit import Xendit, XenditError
+import xendit
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/pay", methods=["POST"])
+def pay():
+    name = request.form.get("name")
+    amount = request.form.get("amount")
+    phone = request.form.get("phone_number")
+    args = {
+        "external_id": f"ovo-pay-{int(time.time())}",
+        "amount": amount,
+        "phone": phone,
+    }
+    xendit_instance = Xendit(api_key=os.getenv("XENDIT_API_KEY"))
+    try:
+        ovo_payment = xendit_instance.EWallet.create_ovo_payment(**args)
+        return vars(ovo_payment)
+    except XenditError as e:
+        return vars(e)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
